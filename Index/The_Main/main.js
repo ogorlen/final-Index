c38e88d00c24adf5eb23e5bc6fd53b39fd51ad02
Oleg <ogorlen@gmail.com>
2019-12-09 22:06:47 -0500

    Create main.js

---
+++ Index/The_Main/main.js
@@ -0,0 +1,11 @@
+function openNav(){
+	document.getElementById("menu").style.width = "250px";
+	// document.getElementById("map").style.marginLeft = "250px";
+	// document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav(){
+	document.getElementById("menu").style.width = "0";
+	// document.getElementById("map").style.marginLeft = "0";
+	// document.body.style.backgroundColor = "white";
+}
